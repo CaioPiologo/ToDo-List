d5cb74980576a063234feee9326a15ae942605b1
Caio Vinícius Piologo Véras Fernandes <caiovpiologo@gmail.com>
2015-03-30 14:24:09 -0300

    TaskWizard adicionado à banda

---
+++ ToDo List/ToDo List/TaskWizard.h
@@ -0,0 +1,32 @@
+//
+//  TaskWizard.h
+//  ToDo List
+//
+//  Created by Caio Vinícius Piologo Véras Fernandes on 3/30/15.
+//  Copyright (c) 2015 Ricardo Z Charf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Task.h"
+
+@interface TaskWizard : NSObject
+-(id)init:(Loader *) loader;
+
+-(void) begin;
+
+-(void) cancel;
+
+-(Task *) finish;
+
+-(void) giveName:(NSString*)name;
+
+-(void) giveInitialDate:(NSDate *)time;
+
+-(void) giveConclusionDate:(NSDate *)time;
+
+-(void) giveDifficulty:(NSNumber*)difficulty;
+
+-(void) giveFun:(NSNumber*)fun;
+
+-(void) hasAlreadyBegun;
+@end
